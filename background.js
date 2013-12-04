9b36e64a9d9d101d366fd58b8bdf06672e49acf5
badpoet <badpoet55@gmail.com>
2013-12-04 20:41:36 +0800

    stuck

--- background.js
+++ background.js
@@ -1,31 +1,67 @@
-var bans = new Array();
-bans[0] = /renren/;
-bans[1] = /weibo/;
-bans[2] = /hupu/;
+// Called when the user clicks on the browser action.
+
+var setHTML = chrome.extension.getURL("settings.html");
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({url: setHTML});
+});    
+
+var bans;
+if (!localStorage.hasOwnProperty('bans')) {
+    bans = new Array();
+}
+else {
+    bans = localStorage.bans;
+}
+
+function appendBans(ban) {
+    bans.push(ban)
+}
+
+function createBan(prefix, pbt, pms, tpms) {
+    // pms = permission
+    // tpms = temporary permission
+    // pbt = prohibition
+    var ban = {
+        prefix : prefix,
+        pbt : pbt,
+        pms : pms,
+        tpms : tpms,
+        silent : false,
+        resume : new Date()
+    };
+    return ban;
+}
 
 var dateTime = new Date();
 var hh = dateTime.getHours();
 var mm = dateTime.getMinutes();
 
 var banHTML = chrome.extension.getURL("ban.html");
+alert(banHTML);
+localStorage.clear();
 
 function checkUrl(tabId, changeInfo, tab) {
-	// check if it's not the ban time
-	if (hh < 1 || hh >= 22 || (hh < 7 && hh >= 6))
-	{
-		return;
-	}
-	else
-	{
-	}
-
-	// check if this page should be banned
-	for (var i = 0, len = bans.length; i < len; i++) {
-		if (bans[i].test(tab.url)) {
-			chrome.tabs.update(tabId, {"url" : banHTML});
-			chrome.pageAction.show(tabId);
-		}
-	}
+    
+    if (!changeInfo.url) {
+        return;
+    }
+    if (setHTML == tab.url) {
+        settingsPrepare(tabId);
+    }
+    var cnt;
+    if (!localStorage.hasOwnProperty('cnt')) {
+        cnt = 0;
+    }
+    else {
+        cnt = parseInt(localStorage.cnt);
+    }
+    cnt = cnt + 1;
+    localStorage.cnt = cnt;
+    alert(cnt);
 };
-
+/*
+function add() {
+    with (document.all) {
+        if (
+*/
 chrome.tabs.onUpdated.addListener(checkUrl);
---
+++ settings.js
@@ -0,0 +1,6 @@
+function ohayo() {
+    alert("fuck");
+}
+
+alert("cao")
+alert(localStorage.cnt);
