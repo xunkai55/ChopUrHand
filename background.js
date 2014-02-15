8726924d9403cf42f1f28a6bbca3ab11e346c875
badpoet <badpoet55@gmail.com>
2014-02-15 11:03:12 +0800

    master - stuck

--- background.js
+++ background.js
@@ -5,48 +5,17 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.create({url: setHTML});
 });    
 
-var bans;
-if (!localStorage.hasOwnProperty('bans')) {
-    bans = new Array();
-}
-else {
-    bans = localStorage.bans;
-}
-
-function appendBans(ban) {
-    bans.push(ban)
-}
-
-function createBan(prefix, pbt, pms, tpms) {
-    // pms = permission
-    // tpms = temporary permission
-    // pbt = prohibition
-    var ban = {
-        prefix : prefix,
-        pbt : pbt,
-        pms : pms,
-        tpms : tpms,
-        silent : false,
-        resume : new Date()
-    };
-    return ban;
-}
-
 var dateTime = new Date();
 var hh = dateTime.getHours();
 var mm = dateTime.getMinutes();
 
 var banHTML = chrome.extension.getURL("ban.html");
 alert(banHTML);
-localStorage.clear();
 
 function checkUrl(tabId, changeInfo, tab) {
     
     if (!changeInfo.url) {
         return;
-    }
-    if (setHTML == tab.url) {
-        settingsPrepare(tabId);
     }
     var cnt;
     if (!localStorage.hasOwnProperty('cnt')) {
@@ -57,7 +26,6 @@ function checkUrl(tabId, changeInfo, tab) {
     }
     cnt = cnt + 1;
     localStorage.cnt = cnt;
-    alert(cnt);
 };
 /*
 function add() {
