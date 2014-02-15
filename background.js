7c7d7ee5486f5214c38e0baa995316ad37d67235
badpoet <badpoet55@gmail.com>
2014-02-15 13:13:38 +0800

    pub@mini

--- background.js
+++ background.js
@@ -1,11 +1,14 @@
 // Called when the user clicks on the browser action.
 
+/*
 var setHTML = chrome.extension.getURL("settings.html");
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.create({url: setHTML});
 });    
+*/
 
 var banHTML = chrome.extension.getURL("red.html");
+alert(banHTML);
 
 var banList = [
     "renren.com/353173410",
@@ -21,20 +24,17 @@ function checkUrl(tabId, changeInfo, tab) {
 
     var dateTime = new Date();
     var hh = dateTime.getHours();
-    if (hh >= 22 || (hh < 7 && hh >= 6)) {
+    if (hh >= 22 || hh < 7) {
         return;
     }
 
     for (var i = 0; i < banList.length; i++)
     {
-        if (banList[i].test(tab.url)) {
-            chrome.tab.update(tabId, {"url" : banHTML});
+        if (tab.url.indexOf(banList[i]) >= 0) {
+            chrome.tabs.update(tabId, {"url" : banHTML});
+            return;
         }
     }
 };
-/*
-function add() {
-    with (document.all) {
-        if (
-*/
+
 chrome.tabs.onUpdated.addListener(checkUrl);
